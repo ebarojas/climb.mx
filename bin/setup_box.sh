b3e1ae53a5281b79272cc5b34e322b32af1048fa
Everardo J Barojas M <everardo@prescrypto.com>
2022-07-26 17:07:22 -0500

    Clean up code

--- bin/setup_box.sh
+++ bin/setup_box.sh
@@ -3,10 +3,9 @@
 echo "=> Start config box..."
 sudo apt-get update
 sudo apt-get install -y git
-sudo python3.6 -m pip install -U pip
-sudo python3.6 -m pip install -U pyOpenSSL
 
 echo "=> Installing requirements..."
-sudo python3.6 -m pip install -r /vagrant/requirements.txt
+# Should install ruby reqs instead
+# sudo python3.6 -m pip install -r /vagrant/requirements.txt
 
 echo "=> End config box..."
